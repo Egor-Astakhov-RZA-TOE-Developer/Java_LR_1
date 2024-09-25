535b08c21f3363ae9f0b54c529d0713e8e1c2d88
astakhov_y <AstakhovYS@mpei.ru>
2024-09-25 17:47:39 +0300

    Created NumberIdentifier

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        String arithmeticExpression = sc.nextLine();
+        String[] arithmeticExpressionSplit = arithmeticExpression.split(" ");
+
+        System.out.println("Program and");
     }
 }
---
+++ src/main/java/org/example/classes/NumberIdentifier.java
@@ -0,0 +1,35 @@
+package org.example.classes;
+
+import java.util.Collections;
+import java.util.List;
+
+public class NumberIdentifier {
+    private final static char[] ROMAN_CHARS = new char[] {'X', 'I', 'V', 'L', 'C'};
+    public static boolean isArabicNumber(String number){
+        // Проверка, что все символы String number это цифры
+        for (int i = 0; i < number.length(); i++) {
+            if (!Character.isDigit(number.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isRomanNumber(String number){
+        // Проверка, что все символы String number это цифры
+        // Цикл по всем символам строки
+        for (int i = 0; i < number.length(); i++) {
+            Character ch = number.charAt(i);
+            boolean isRoman = false;
+
+            // цикл по всем символам массива  ROMAN_CHARS
+            for (char charRoman: ROMAN_CHARS){
+                if (ch.equals(charRoman)) isRoman = true;
+            }
+            if (!isRoman){
+                return false;
+            }
+        }
+        return true;
+    }
+}
